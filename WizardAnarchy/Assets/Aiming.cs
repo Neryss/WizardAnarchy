8411f33a0db9f9975c6ee38037a95bf13e33f3f2
neryss <kuroysiris@gmail.com>
2020-04-21 19:35:38 +0200

    Basic player movement and aiming WIP

---
+++ WizardAnarchy/Assets/Aiming.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Aiming : MonoBehaviour
+{
+    public Camera cam;
+    private Vector2 mousePos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("mouse pos :" + mousePos);
+    }
+}
