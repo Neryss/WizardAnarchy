17ea0da7dc4c27848339b0dbc511daabf8105cf3
neryss <kuroysiris@gmail.com>
2020-04-21 18:30:54 +0200

    Init commit

---
+++ WizardAnarchy/Assets/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float BASE_MOVE_SPEED;
+    public Vector2 moveInput;
+    public Rigidbody2D rb2D;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(this)
+        {
+            MovePlayer();
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if(this)
+        {
+            moveInput.x = Input.GetAxisRaw("Horizontal");
+            moveInput.y = Input.GetAxisRaw("Vertical");
+        }
+    }
+
+    private void MovePlayer()
+    {
+        rb2D.velocity = new Vector2(moveInput.x * BASE_MOVE_SPEED, moveInput.y * BASE_MOVE_SPEED);
+    }
+}
