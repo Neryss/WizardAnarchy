e332d9fed05f44c0dc9743d6338475c3fddcc2eb
neryss <kuroysiris@gmail.com>
2020-04-22 18:03:22 +0200

    Added enemy collisions for firespell and destroy on trigger

--- WizardAnarchy/Assets/FlameScript.cs
+++ WizardAnarchy/Assets/FlameScript.cs
@@ -4,20 +4,17 @@ using UnityEngine;
 
 public class FlameScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    void OnTriggerEnter2D(Collider2D col)
     {
-        
+        if(col.CompareTag("Enemy"))
+        { 
+            Debug.Log("Entered a trigger");
+            DestroyProjectile();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void DestroyProjectile()
     {
-        
-    }
-
-    void OnTriggerEnter2D(Collision2D col)
-    {
-        
+        Destroy(gameObject);
     }
 }
