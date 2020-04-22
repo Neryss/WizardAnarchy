9bb8eda3661a4603a0d4edc23c9e0a3841ee62ac
neryss <kuroysiris@gmail.com>
2020-04-23 01:28:27 +0200

    Added a really basic enemy movement

---
+++ WizardAnarchy/Assets/DistEnemyController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DistEnemyController : MonoBehaviour
+{
+    public Rigidbody2D rb2D;
+    public Transform target;
+    public float moveSpeed;
+    public float minRange;
+    public float maxRange;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveEnemy();
+    }
+
+    private void MoveEnemy()
+    {
+        Vector2 movePos = target.position - transform.position;
+        rb2D.velocity = movePos * moveSpeed;
+    }
+}
