9f05cc7be744f925bee88ba1e51688dd753b4f9a
neryss <kuroysiris@gmail.com>
2020-04-22 14:00:12 +0200

    Shooting script done, not sure if the bullet will be trigger or has actual physics

--- WizardAnarchy/Assets/PlayerShooting.cs
+++ WizardAnarchy/Assets/PlayerShooting.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class PlayerShooting : MonoBehaviour
 {
-    public GameObject bullet;
+    public GameObject bulletPrefab;
     public Transform firePoint;
-    public float bulletSpeed;
+    public float bulletForce;
+    public Rigidbody2D bulletRb;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +17,16 @@ public class PlayerShooting : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            PlayerShot();
+        }
+    }
+
+    private void PlayerShot()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, Quaternion.identity);
+        bulletRb = bullet.GetComponent<Rigidbody2D>();
+        bulletRb.AddForce(firePoint.right * bulletForce, ForceMode2D.Impulse);
     }
 }
