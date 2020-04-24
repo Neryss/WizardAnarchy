254ac99b02f98f649de777c5e138091993b9359e
neryss <kuroysiris@gmail.com>
2020-04-24 23:33:58 +0200

    Finally know how to use a script from another obj

---
+++ WizardAnarchy/Assets/PlayerHealth.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int maxHealth;
+    public int health;
+    public EnemyHealth eHealth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(health <= 0)
+        {
+            eHealth.KillEntity(gameObject);
+        }
+    }
+
+    public void PlayerTakeDamage(int damage)
+    {
+        health =- damage;
+    }
+}
---
+++ WizardAnarchy/Assets/Scripts/Enemies_scripts/mage/MageAiming.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MageAiming : MonoBehaviour
+{
+    public Transform target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 lookDir = target.position - transform.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, angle);
+    }
+}
