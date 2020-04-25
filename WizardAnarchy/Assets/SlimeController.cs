c3e60a086667bbad69567df38a11c97a1020ff2f
neryss <kuroysiris@gmail.com>
2020-04-25 18:25:58 +0200

    Added slimeController, not dure if I'll keep it or use the mage one since they're kinda the same

--- WizardAnarchy/Assets/SlimeController.cs
+++ WizardAnarchy/Assets/SlimeController.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class SlimeController : MonoBehaviour
 {
+    public float speed;
+    public float range;
+    public Transform target;
+    public Rigidbody2D slimeRB;
+    private Vector2 direction;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,22 @@ public class SlimeController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(target)
+        {
+            SlimeMove();
+        }
+    }
+
+    void FixedUpdate()
+    {
+        direction = target.position - transform.position;
+    }
+
+    private void SlimeMove()
+    {
+        if(Vector2.Distance(transform.position, target.position) >= range)
+        {
+            slimeRB.MovePosition(slimeRB.position + direction * speed * Time.deltaTime);
+        }
     }
 }
