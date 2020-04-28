e02fa070e6f440575493170ca83c94d26638cfdb
neryss <kuroysiris@gmail.com>
2020-04-28 13:12:17 +0200

    It works

---
+++ WizardAnarchy/Assets/Scripts/Player/Aiming.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Aiming : MonoBehaviour
+{
+    public Camera cam;
+    public Vector2 mousePos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 lookDir = new Vector2(mousePos.x - transform.position.x, mousePos.y - transform.position.y);
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, angle);
+    }
+}
---
+++ WizardAnarchy/Assets/Scripts/Player/PlayerShooting.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShooting : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public GameObject fireMuzzle;
+    public Transform firePoint;
+    public float bulletForce;
+    private Rigidbody2D bulletRb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+ 
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            PlayerShot();
+        }
+    }
+
+    private void PlayerShot()
+    {
+        GameObject tempMuzzle = Instantiate(fireMuzzle, firePoint.position, Quaternion.identity);
+        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, Quaternion.identity);
+        bulletRb = bullet.GetComponent<Rigidbody2D>();
+        bulletRb.AddForce(firePoint.right * bulletForce, ForceMode2D.Impulse);
+        Destroy(tempMuzzle, 2);
+    }
+}
