658c80eea1d31b04ad0986becec102a860374a3f
Neryss <kuroysiris@gmail.com>
2020-04-28 12:59:22 +0200

    Started from scratch
    New test

---
+++ WizardAnarchy0.1/Assets/Scripts/Enemies/Mage/MageFiring.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MageFiring : MonoBehaviour
+{
+    public Transform firePoint;
+    public GameObject bulletPrefab;
+    public float fireCd;
+    private float cdCount;
+    public float bulletSpeed;
+    private Rigidbody2D bulletRB;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cdCount = fireCd;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(cdCount <= 0)
+        {
+            cdCount = fireCd;
+            EnemyShoot();
+        }
+        else
+        {
+            cdCount -= Time.deltaTime;
+        }
+    }
+
+    private void EnemyShoot()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, Quaternion.identity);
+        bulletRB = bullet.GetComponent<Rigidbody2D>();
+        bulletRB.AddForce(firePoint.right * bulletSpeed, ForceMode2D.Impulse);
+    }
+}
---
+++ WizardAnarchy0.1/Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public GameManager gameManager;
+    public int maxHealth;
+    public int health;
+    public EnemyHealth eHealth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(health <= 0)
+        {
+            eHealth.KillEntity(gameObject);
+            gameManager.GameOver();
+        }
+    }
+
+    public void PlayerTakeDamage(int damage)
+    {
+        health -= damage;
+    }
+}
