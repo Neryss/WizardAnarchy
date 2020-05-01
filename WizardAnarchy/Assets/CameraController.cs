3e29a1a121c5a2cb15e04c2fd658667f20c06a2c
neryss <kuroysiris@gmail.com>
2020-05-02 01:01:48 +0200

    Added camera controller with a kinda ETG system, need to smooth the lerp and add boundaries

---
+++ WizardAnarchy/Assets/CameraController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform player;
+    public Vector3 offset;
+    private Vector3 center;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        center = new Vector3((player.position.x + mousePos.x) / 2, (player.position.y + mousePos.y) / 2, 0f);
+        transform.position = Vector3.Lerp(transform.position, center + offset, Time.deltaTime);
+        //need to add some boundaries so the camera only follow the cursor in certain coniditions
+        //also add a float smooth but i'm tired
+    }
+}
