4c05fccc7ccfcdf609d8cb6c7e56824f8e001df4
neryss <kuroysiris@gmail.com>
2020-05-01 19:46:53 +0200

    Added a first try for the camera system but it sucks

---
+++ WizardAnarchy/Assets/FollowCursor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCursor : MonoBehaviour
+{
+    public Vector3 newPos;
+    public Transform player;
+    public Vector3 cursorPos;
+    public Vector3 offset;
+    public Camera cam;
+    void Start()
+    {
+           
+    }
+
+    void FixedUpdate()
+    {
+        cursorPos = cam.ScreenToWorldPoint(Input.mousePosition);
+        newPos = (cursorPos - player.position) / 2;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = newPos + offset;
+    }
+}
--- WizardAnarchy/Assets/ShakeCameraController.cs
+++ WizardAnarchy/Assets/ShakeCameraController.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class ShakeCameraController : MonoBehaviour
 {
     public float shakeDuration;
+    public float shakeDurationCount;    //set it to public for debugging
+    public float shakePower;
+    public float rotation;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +17,29 @@ public class ShakeCameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown("k"))
+        {
+            StartShake(3f, 1f);
+        }
+    }
+
+    void LateUpdate()
+    {
+        if(shakeDurationCount > 0)
+        {
+            shakeDurationCount -= Time.deltaTime;
+
+            float xAmount = Random.Range(-1f, 1f) * shakePower;
+            float yAmount = Random.Range(-1f, 1f) * shakePower;
+            //Can add another value for the zoom level and rotation
+
+            transform.position += new Vector3(xAmount, yAmount, 0f);
+        }
+    }
+
+    public void StartShake(float duration, float power)
+    {
+        shakeDurationCount = duration;
+        shakePower = power;
     }
 }
