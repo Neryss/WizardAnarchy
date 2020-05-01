522f9b3bad0551920d8aa2f9c54a0a8164855715
neryss <kuroysiris@gmail.com>
2020-05-01 16:51:43 +0200

    Angle shit solved ffs, need to clear the effects and it's done

--- WizardAnarchy/Assets/Scripts/Player/FlameScript.cs
+++ WizardAnarchy/Assets/Scripts/Player/FlameScript.cs
@@ -15,14 +15,7 @@ public class FlameScript : MonoBehaviour
     public int damage;
     void Start()
     {
-        pShooting = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerShooting>();
-        rotationTrans = GameObject.FindGameObjectWithTag("Player").transform.GetChild(0);
-        //need to reference the rotation of the chil rotationPoint so I can apply it to the effect
-        //Not getting the wanted rotation (got the z since it's the only one changing on the rotation point so doesn't work)
-        print(rotationTrans.transform.rotation.z);
-        print("x rotat" + rotationTrans.transform.rotation.x);
-        firePoint = pShooting.firePoint.transform;
-        Instantiate(muzzleEffect, firePoint.position + new Vector3(0, 0, -1), Quaternion.Euler(rotationTrans.rotation.x, rotationTrans.rotation.y, rotationTrans.rotation.z));
+        SpawnMuzzle();
     }
     void OnTriggerEnter2D(Collider2D col)
     {
@@ -40,5 +33,13 @@ public class FlameScript : MonoBehaviour
     private void DestroyProjectile()
     {
         Destroy(gameObject);
+    }
+
+    private void SpawnMuzzle()
+    {
+        pShooting = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerShooting>();
+        rotationTrans = GameObject.FindGameObjectWithTag("Player").transform.GetChild(0);
+        firePoint = pShooting.firePoint.transform;
+        Instantiate(muzzleEffect, firePoint.position + new Vector3(0, 0, -1), Quaternion.LookRotation(firePoint.position - rotationTrans.position));
     }
 }
