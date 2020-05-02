da3c37e5d4f944e55e7b1f0614242cd385ab0c8c
neryss <kuroysiris@gmail.com>
2020-05-03 00:18:33 +0200

    Fixed crosshair jiggling with camera movement and attached it to the camera script instead of the aiming one

--- WizardAnarchy/Assets/CameraController.cs
+++ WizardAnarchy/Assets/CameraController.cs
@@ -4,22 +4,38 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    public GameObject cursor;
+    private GameObject cursorInstance;
     public Transform player;
+    private Vector3 mousePos;
     public Vector3 offset;
     private Vector3 center;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.visible = false;
+        cursorInstance = Instantiate(cursor, new Vector3(transform.position.x, transform.position.y, -2), Quaternion.identity);
+    }
+
+    void FixedUpdate()
+    {
+        FollowPlayer();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        center = new Vector3((player.position.x + mousePos.x) / 2, (player.position.y + mousePos.y) / 2, 0f);
-        transform.position = Vector3.Lerp(transform.position, center + offset, Time.deltaTime);
-        //need to add some boundaries so the camera only follow the cursor in certain coniditions
-        //also add a float smooth but i'm tired
+        MoveCursor();
+    }
+
+    private void FollowPlayer()
+    {
+        transform.position = player.position + offset;
+    }
+
+    private void MoveCursor()
+    {
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        cursorInstance.transform.position = mousePos + new Vector3(0f, 0f, 3);
     }
 }
