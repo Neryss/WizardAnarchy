1b6baddc65c2a45c96ea02b730f5c2b13d13faa4
neryss <kuroysiris@gmail.com>
2020-05-24 19:02:38 +0200

    Started the grid script used for the room generation

---
+++ WizardAnarchy/Assets/Scripts/GridScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridScript {
+    private int width;
+    private int height;
+    private int[,] gridArray;
+
+    public void Grid(int width, int height) {
+        this.width = width;
+        this.height = height;
+
+        gridArray = new int[width, height];
+    }
+}
