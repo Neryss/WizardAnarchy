817852723c09c42fbb425a633c9409a75b34e073
neryss <kuroysiris@gmail.com>
2020-05-25 23:37:59 +0200

    Got an random room generation running, with its constructor

--- WizardAnarchy/Assets/Scripts/GridTestingScript.cs
+++ WizardAnarchy/Assets/Scripts/GridTestingScript.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class GridTestingScript : MonoBehaviour
 {
+    public GameObject[] rooms;
     void Start()
     {
-        CustomGrid cGrid = new CustomGrid(5, 5, 39);
+        CustomGrid cGrid = new CustomGrid(5, 5, 39, rooms);
     }
 }
