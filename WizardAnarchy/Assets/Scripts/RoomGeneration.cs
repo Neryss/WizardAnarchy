5435e952fbf16b447dc3cb9616e6a9dcde4c4691
neryss <kuroysiris@gmail.com>
2020-05-26 15:25:44 +0200

    Removed the class constructor for the random gen

--- WizardAnarchy/Assets/Scripts/RoomGeneration.cs
+++ WizardAnarchy/Assets/Scripts/RoomGeneration.cs
@@ -4,13 +4,9 @@ using UnityEngine;
 
 public class RoomGeneration : MonoBehaviour
 {
-    public int gridWidth;
-    public int gridHeight;
-    public float cellSize;
     public GameObject[] rooms;
-    void Start()
-    {
-        //CustomGrid cGrid = new CustomGrid(gridWidth, gridHeight, cellSize, rooms);
-        RandomRoomGeneration randomGen = new RandomRoomGeneration(gridWidth, gridHeight, cellSize, rooms);
+    
+    private void Start() {
+        
     }
 }
