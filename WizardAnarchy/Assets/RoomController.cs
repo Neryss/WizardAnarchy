d78bc880ae0da55bf977ecd32ad42b930afda4e4
neryss <kuroysiris@gmail.com>
2020-05-26 16:38:05 +0200

    Reworked room prefab

---
+++ WizardAnarchy/Assets/RoomController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomController : MonoBehaviour
+{
+    private bool leftDoor;
+    private bool rightDoor;
+    private bool upDoor;
+    private bool downDoor;
+    void Start()
+    {
+        
+    }
+}
