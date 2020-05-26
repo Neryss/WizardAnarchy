660fd6bfbe313e08823f170f7f187bfcc9f106a7
neryss <kuroysiris@gmail.com>
2020-05-26 18:06:07 +0200

    Tweaked prefab and added initRoom for the gen

--- WizardAnarchy/Assets/Scripts/RoomGeneration.cs
+++ WizardAnarchy/Assets/Scripts/RoomGeneration.cs
@@ -6,7 +6,20 @@ public class RoomGeneration : MonoBehaviour
 {
     public GameObject[] rooms;
     
-    private void Start() {
-        
+    private void Awake() {
+        InitFirstRoom();
+    }
+
+    private GameObject RandomRoomSelection(GameObject[] roomArray)
+    {
+        int i = Random.Range(0, roomArray.Length);
+        GameObject selectedRoom = roomArray[i];
+
+        return(selectedRoom);
+    }
+
+    private void InitFirstRoom()
+    {
+        Instantiate(RandomRoomSelection(rooms), transform.position, Quaternion.identity);
     }
 }
