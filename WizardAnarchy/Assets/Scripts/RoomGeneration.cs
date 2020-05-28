6a55252e76e4ac2dc0005bac89d3ce9ca130790d
neryss <kuroysiris@gmail.com>
2020-05-28 18:10:50 +0200

    Added a timer for the spawn

--- WizardAnarchy/Assets/Scripts/RoomGeneration.cs
+++ WizardAnarchy/Assets/Scripts/RoomGeneration.cs
@@ -9,15 +9,25 @@ public class RoomGeneration : MonoBehaviour
     public RoomController roomController;
     private int roomCount;
     public int maxRoom;
+    private float timeBtwSpawn;
+    public float maxTimeBtwSpawn = 0.25f;
     
     private void Start() {
         //Spawns the first room at the generator position
         roomBuffer = GameObject.Instantiate(RandomRoomSelection(rooms), transform.position, Quaternion.identity);
 
-        roomController = roomBuffer.GetComponent<RoomController>();
-        Move();
+        //I'll take care of the condition for spawning later
+        //roomController = roomBuffer.GetComponent<RoomController>();
     }
 
+    private void Update() {
+        roomBuffer = RandomRoomSelection(rooms);
+        if(timeBtwSpawn <= 0)
+        {
+            timeBtwSpawn = maxTimeBtwSpawn;
+            Move();
+        }
+    }
     private GameObject RandomRoomSelection(GameObject[] roomArray)
     {
         int i = Random.Range(0, roomArray.Length);
