5e8db1f34722735f01ab99b152cb01d94d78a604
neryss <kuroysiris@gmail.com>
2020-06-05 02:07:50 +0200

    Working grid on script with vector array so maybe I coul track visited pos

--- WizardAnarchy/Assets/GridRoomGenerator.cs
+++ WizardAnarchy/Assets/GridRoomGenerator.cs
@@ -5,11 +5,16 @@ using UnityEngine;
 public class GridRoomGenerator : MonoBehaviour
 {
     private int[,] gridArray;
+    private Vector2[] vectorArray;
     public GameObject[] roomArray;
     // Start is called before the first frame update
     void Start()
     {
-        
+        vectorArray = InitGrid(5, 5, 29, 19);
+        for(int i = 0; i < vectorArray.Length; i++)
+        {
+            Debug.Log("Coordinates are : " + vectorArray[i]);
+        }
     }
 
     // Update is called once per frame
@@ -18,8 +23,23 @@ public class GridRoomGenerator : MonoBehaviour
         
     }
 
-    private void InitGrid(int width, int height, int xSize, int ySize)
+    private Vector2[] InitGrid(int width, int height, int xSize, int ySize)
     {
-        gridTiles
+        gridArray = new int[width, height];
+        Vector2[] coordArray = new Vector2[gridArray.Length];
+        int i = 0;
+
+        for(int x = 0; x < gridArray.GetLength(0); x++)
+        {
+            for(int y = 0; y < gridArray.GetLength(1); y++)
+            {
+                float posX = x * xSize;
+                float posY = y * -ySize;
+                Vector2 posVector = new Vector2(posX, posY);
+                coordArray[i] = posVector;
+                i++;
+            }
+        }
+        return(coordArray);
     }
 }
